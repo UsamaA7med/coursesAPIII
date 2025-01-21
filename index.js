5fcca61e3b3ad1ccee26a21d2c10680f25aa9f3f
UsamaA7med <osamaahmedwork1@gmail.com>
2025-01-21 11:21:03 +0200

    dcxs

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use("/", (req, res, next) => {
+  console.log("request came vericle started");
+  next();
+});
+
 app.use("/api/courses", coursesRouter);
 
 app.use("/api/users", usersRouter);
